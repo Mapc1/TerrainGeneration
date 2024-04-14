25cac46df8f42b63f689fdd104507a8759dd2320
PWACN <alex.pedro.sousa@gmail.com>
2024-04-14 18:29:22 +0100

    newCurve

--- shaders/curves/TerrainCurves.glsl
+++ shaders/curves/TerrainCurves.glsl
@@ -5,12 +5,15 @@ float getPeaksNValleys(float noise, float factor);
 float getContinental(float noise, float factor){
     
     float displacement = 0;
-    if(noise <= 0.3) displacement = -1;
-    else if (noise <= 0.45) displacement = 5 * noise - 2.5;
-    else if (noise <= 0.55) displacement = 2.5 * noise - 1.375;
-    else if (noise <= 0.65) displacement = 1 * noise - 0.55;
-    else if (noise <= 0.80) displacement = (7/3) * noise - (17/12);
-    else if (noise <= 0.95) displacement = (11/3) * noise - (149/60);
+    if(noise <= -0.6) displacement = -1;
+    else if (noise <= -0.5) displacement = noise * 6 - 2.6;
+    else if (noise <= -0.2) displacement = noise * 1 - 0.1;
+    else if (noise <= -0.05) displacement = noise * (2/3) - (1/30);
+    else if (noise <= 0.1) displacement = noise * (1/3) - (1/60);
+    else if (noise <= 0.4) displacement = noise * 0.5 + 0;
+    else if (noise <= 0.6) displacement = noise * 1.5 + 0.4;
+    else if (noise <= 0.75) displacement = noise * (5/3) + 0.5;
+    else if (noise <= 0.9) displacement = noise * (5/3) + 0.5;
     else displacement == 1;
 
     return displacement * factor;
